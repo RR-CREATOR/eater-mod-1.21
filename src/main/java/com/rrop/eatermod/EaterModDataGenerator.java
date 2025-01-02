fbcd00a3cc7a60209c4d7009421937ea23047bd0
RR-CREATOR <rian.sanghi2009@gmail.com>
2025-01-03 03:52:30 +0530

    Initial commit

---
+++ src/main/java/com/rrop/eatermod/EaterModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rrop.eatermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EaterModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
